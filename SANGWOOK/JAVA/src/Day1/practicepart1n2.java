384108e555d17c3ffbc9ee72b2e16c525cd38067
sangwook <sangwook.co.kr>
2020-09-04 18:13:11 +0900

    2020.09.04

---
+++ SANGWOOK/JAVA/src/Day1/practicepart1n2.java
@@ -0,0 +1,52 @@
+package Day1;
+
+public class practicepart1n2 {
+	public static void main(String[] args) {
+
+		//문제1
+		int bottom = 5;
+		int top = 7;
+		
+		int tri = (bottom * top) / 2;
+		System.out.println(tri);  
+		
+		//문제2
+		int sec = 250;
+		float minute = sec /60f ;
+		System.out.println(minute);
+		
+		//문제 3
+		/*
+		 * 3
+		 * 	1. 18
+		 *  2. 4
+		 *  3. false
+		 *  4. 2
+		 *  5. 66  
+		 *  6. 66  B
+		 *  7. 66  B
+		 *  8. 65  C
+		 * */
+		
+		
+		//문제 4
+		
+		/*
+		 * 11. boolean형은 형변환이 안된다 그래서 int 값을 가질 수 없다  오직 false나 true만 값으로 가능하다
+		 * 12. 11 
+		 * 13. 15
+		 * 14. zA10 
+		 *
+		 */
+	}
+}
+
+
+
+
+
+
+
+
+
+
